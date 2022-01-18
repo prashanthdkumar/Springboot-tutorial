13a010c762b0eadd6fff7c91dbc1776b8d6df7c3
Prashanth <prashanthdkumar88@gmail.com>
2022-01-17 23:36:21 -0500

    Using Lombok (to remove boiler plate code)

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/entity/Department.java
@@ -1,24 +1,21 @@
 package com.dailycodebuffer.Springboot.tutorial.entity;
 
-import org.hibernate.validator.constraints.Length;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.validation.constraints.Email;
-import javax.validation.constraints.Future;
-import javax.validation.constraints.FutureOrPresent;
-import javax.validation.constraints.Negative;
-import javax.validation.constraints.NegativeOrZero;
 import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.PastOrPresent;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
-import javax.validation.constraints.Size;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Department {
     
     @Id
@@ -41,55 +38,4 @@ public class Department {
     private String departmentAddress;
     private String departmentCode;
     
-    public Long getDepartmentId() {
-        return departmentId;
-    }
-    
-    public void setDepartmentId(Long departmentId) {
-        this.departmentId = departmentId;
-    }
-    
-    public String getDepartmentName() {
-        return departmentName;
-    }
-    
-    public void setDepartmentName(String departmentName) {
-        this.departmentName = departmentName;
-    }
-    
-    public String getDepartmentAddress() {
-        return departmentAddress;
-    }
-    
-    public void setDepartmentAddress(String departmentAddress) {
-        this.departmentAddress = departmentAddress;
-    }
-    
-    public String getDepartmentCode() {
-        return departmentCode;
-    }
-    
-    public void setDepartmentCode(String departmentCode) {
-        this.departmentCode = departmentCode;
-    }
-    
-    public Department(Long departmentId, String departmentName, String departmentAddress, String departmentCode) {
-        this.departmentId = departmentId;
-       this.departmentName = departmentName;
-        this.departmentAddress = departmentAddress;
-        this.departmentCode = departmentCode;
-    }
-    
-    public Department() {
-    }
-    
-    @Override
-    public String toString() {
-        return "Department{" +
-            "departmentId=" + departmentId +
-            ", departmentName='" + departmentName + '\'' +
-            ", departmentAddress='" + departmentAddress + '\'' +
-            ", departmentCode='" + departmentCode + '\'' +
-            '}';
-    }
 }
