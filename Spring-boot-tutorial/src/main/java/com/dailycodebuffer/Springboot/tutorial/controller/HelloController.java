576a2747144d6e42a063cf718d76495c25b7e40a
Prashanth <prashanthdkumar88@gmail.com>
2022-01-19 00:02:05 -0500

    Add configuration in property files and read it using @Value in controller

--- Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/HelloController.java
+++ Spring-boot-tutorial/src/main/java/com/dailycodebuffer/Springboot/tutorial/controller/HelloController.java
@@ -1,14 +1,18 @@
 package com.dailycodebuffer.Springboot.tutorial.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
     
+    @Value("${welcome.message}")
+    private String welcomeMessage;
+    
     @GetMapping("/")
     public String helloWorld() {
-        return "Welcome to my Spring Boot project..!";
+        return welcomeMessage;
     }
     
 }
